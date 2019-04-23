9e128c743e868fbc8558b4e4f8c45dd5bdee6600
Sean Choon <38978508+seanisoverhere@users.noreply.github.com>
2019-04-23 14:11:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // Changing colour of navbar when scrolling
-$(function () {
-  $(document).scroll(function () {
-    var $nav = $(".navbar-fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $(".navbar").addClass("scrolled");
+    }
+    else {
+      $(".navbar").removeClass("scrolled");
+    }
   });
-});
